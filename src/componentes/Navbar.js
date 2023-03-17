11e278722ab79897510a5935fb53b5371e7bb483
Lanitakun <lana.tomi.gabriel@gmail.com>
2023-03-17 19:17:37 -0300

    actualizado

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,48 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../styles/Navbar.css";
 import logo from "../img/logo.png";
 
 export function Navbar() {
-  const [scrolled, setScrolled] = useState(false);
-  const [showMenu, setShowMenu] = useState(false);
 
-  const handleScroll = () => {
-    const scrollTop = window.pageYOffset;
-    if (scrollTop >= 0 && !scrolled) {
-      setScrolled(true);
-    } else if (scrollTop <= 0 && scrolled) {
-      setScrolled(false);
-    }
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.pageYOffset;
-      if (scrollTop >= 0 && !scrolled) {
-        setScrolled(true);
-      } else if (scrollTop <= 0 && scrolled) {
-        setScrolled(false);
-      }
-    };
-    
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [scrolled]);
-
+  
   return (
-      <><button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button><div className={`collapseNavbarCollapse ${scrolled ? "scrolled" : ""}`} id="navbarNav">
+      <div className= "collapseNavbarCollapse" id="navbarNav">
         <a className="logo" href="#presentacion">
           <img src={logo} alt="Logo de Tobalaba Marka" style={{ width: "70px", height: "auto", margin: "10px" }} />
         </a>
@@ -60,6 +24,6 @@ export function Navbar() {
             <a className="nav-link disabled" href="www.lanitakun.me">Contacto</a>
           </li>
         </ul>
-      </div></>
-  );
+      </div>
+      )
 }
