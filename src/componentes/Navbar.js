5a95285a6786ab1c3abda349c326fbf29d0dee55
Lanitakun <lana.tomi.gabriel@gmail.com>
2023-03-17 19:10:01 -0300

    actualizado

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -46,16 +46,6 @@ export function Navbar() {
         <a className="logo" href="#presentacion">
           <img src={logo} alt="Logo de Tobalaba Marka" style={{ width: "70px", height: "auto", margin: "10px" }} />
         </a>
-        <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-        onClick={toggleMenu}
-        ></button>
         <ul className="navbar-nav">
           <li className="nav-item active">
             <a className="nav-link" href="www.lanitakun.me">Presentación <span className="sr-only"></span></a>
