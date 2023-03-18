f418a5424dc5bb0d1efdf51fefe73a81e5436c70
Lanitakun <lana.tomi.gabriel@gmail.com>
2023-03-18 17:21:15 -0300

    Se agrego el post de instagram

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
       <div className="ContainerPrincipal">
       <h1 className='titulo' id='presentacion'>Presentación</h1>
       <Presentacion />
+      <h1 className='titulo' id='instagram'>Instagram</h1>
+      <iframe className='frameInstagram' title='PostInstagram' width="auto" height="500" src="https://www.instagram.com/p/Cp0aOBAOO0W/embed" frameborder="0"></iframe>
       <h1 className='titulo' id='plataformas'>Escuchanos</h1>
       <Plataformas />
       <h1 className='titulo' id='contacto'>Contacto</h1>
--- src/componentes/Presentacion.js
+++ src/componentes/Presentacion.js
@@ -17,7 +17,11 @@ export function Presentacion() {
         <p className="texto-presentacion">
         Nació en el año 2000 como un taller de danza y música folclórica en la Escuela Tobalaba de Peñalolén. Su director, el profesor Mauricio Varela, decidió iniciar la música y las danzas andinas en 2001. Tobalaba-Marka ha mantenido un trabajo constante en el desarrollo cultural de danzas andinas, explorando nuevas alternativas musicales con creaciones propias de carácter progresivo, sin perder el alma rítmica de las danzas. Ha realizado más de 560 presentaciones en vivo en diversos escenarios, que van desde peñas folclóricas hasta grandes eventos, compartiendo el escenario con artistas destacados de otras áreas. Cabe mencionar que todas las canciones de ambos grupos artísticos son de autoría propia, sumando a la fecha 48 composiciones musicales y 12 danzas.
         </p>
+        
       </div>
+      
+           
+      
 
     </div>
   );
