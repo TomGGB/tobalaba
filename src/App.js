d18a303279b8b4be7870f6dbb419d7337445d0f1
Lanitakun <82401637+Lanitakun@users.noreply.github.com>
2023-03-27 12:09:18 -0300

    cambio hecho

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   }
 
   peticionGet = () => {
+    //variable de entorno
     const dbUrl = process.env.REACT_APP_DB_URL;
     fetch(dbUrl)
       .then((response) => response.json())
