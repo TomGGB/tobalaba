83635f18968e0a63aac430c8bbf0b88ee2fa5e9f
Tomas <lana.tomi.gabriel@gmail.com>
2023-08-15 10:15:44 -0400

    prese

--- src/componentes/Presentacion.js
+++ src/componentes/Presentacion.js
@@ -17,9 +17,8 @@ export function Presentacion() {
       <div className="div-presentacion">
         <h1 className="titulo-presentacion">Tobalaba Marka</h1>
         <p className="texto-presentacion">
-        Nació en el año 2000 como un taller de danza y música folclórica en la Escuela Tobalaba de Peñalolén. Su director, el profesor Mauricio Varela, decidió iniciar la música y las danzas andinas en 2001. Tobalaba-Marka ha mantenido un trabajo constante en el desarrollo cultural de danzas andinas, explorando nuevas alternativas musicales con creaciones propias de carácter progresivo, sin perder el alma rítmica de las danzas. Ha realizado más de 560 presentaciones en vivo en diversos escenarios, que van desde peñas folclóricas hasta grandes eventos, compartiendo el escenario con artistas destacados de otras áreas. Cabe mencionar que todas las canciones de ambos grupos artísticos son de autoría propia, sumando a la fecha 48 composiciones musicales y 12 danzas.
+        Surgió iniciales del año 2000 como un centro de enseñanza de danzas y música folclórica en la Escuela Tobalaba de Peñalolén. Su líder, el maestro Mauricio Varela, optó por introducir melodías y bailes andinos en 2001. Tobalaba-Marka ha mantenido una dedicación constante al enriquecimiento cultural de las danzas andinas, explorando nuevas direcciones musicales mediante creaciones originales de estilo progresivo, sin abandonar la esencia rítmica inherente a las danzas. Ha llevado a cabo más de 560 presentaciones en directo en una variedad de escenarios, desde reuniones folclóricas hasta eventos de gran magnitud, compartiendo escenario con destacados artistas de otras disciplinas. Es relevante señalar que todas las canciones de ambos grupos artísticos son de autoría exclusiva, sumando hasta la fecha un total de 48 composiciones musicales y 12 danzas propias.        
         </p>
-        
       </div>
       
            
